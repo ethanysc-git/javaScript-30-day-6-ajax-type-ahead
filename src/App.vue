8e41620d5d75ff5f2b5a5ab9751ba9a5e402ec8e
Ethan Chen <acuratnam@gmail.com>
2024-06-03 10:53:27 +0800

    added name

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@ import SearchBox from './components/SearchBox.vue';
 
 <template>
   <div>
+    <a href="https://github.com/ethanysc-git/javaScript-30-day-6-ajax-type-ahead" target="_blank">
+      <h1>ethanysc-git</h1>
+    </a>
+  </div>
+  <div>
     <SearchBox />
   </div>
 </template>
